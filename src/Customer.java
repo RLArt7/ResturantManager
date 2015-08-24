6e2f30367b02b3ab6976fe706a59b535f2cb4e33
Harel Avikasis <harelavikasis@gmail.com>
2015-08-24 19:18:42 +0300

    Add annotations and methods
    add methods to customer and notation classes

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 
 public class Customer extends Person{
 	
@@ -17,6 +18,58 @@ public class Customer extends Person{
 	public String toString() {
 		return "Customer: "+this.getName() + "  whileWaiting: " + whileWaiting;
 	}
+	
+	@WhileWaiting
+	public void readNewsPaper() throws InterruptedException {
+		long actionTime = (long) (Math.random() * 10000);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " start reading a paper for" + actionTime
+				+ "ms");
+		Thread.sleep(actionTime);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " finished reading");
+	}
+	@WhileWaiting
+	public void playBubbles() throws InterruptedException {
+		long actionTime = (long) (Math.random() * 10000);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " start play Bubbles for" + actionTime
+				+ "ms");
+		Thread.sleep(actionTime);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " finished playing");
+	}
+	@WhileWaiting
+	public void doHomework() throws InterruptedException {
+		long actionTime = (long) (Math.random() * 10000);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " start doing Homework for" + actionTime
+				+ "ms");
+		Thread.sleep(actionTime);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " finished Homework");
+	}
+	@NotAllowed
+	public void talkOnThePhone() throws InterruptedException {
+		long actionTime = (long) (Math.random() * 10000);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " start talknig on the phone for" + actionTime
+				+ "ms");
+		Thread.sleep(actionTime);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " finished talking");
+	}
+	@NotAllowed
+	public void whatchMovie() throws InterruptedException {
+		long actionTime = (long) (Math.random() * 10000);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " start watching a movie for" + actionTime
+				+ "ms");
+		Thread.sleep(actionTime);
+		System.out.println(Calendar.getInstance().getTimeInMillis()
+				+ " Customer #" + this.getName() + " finished watching");
+	}
+	
 	
 	
 }
---
+++ src/WhileWaiting.java
@@ -0,0 +1,8 @@
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WhileWaiting {
+	//		String value();
+	//		int    estimatedEffortTime() default 20;
+}
